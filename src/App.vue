9abcf5d3d86d1331467f0c35099c9f99b8a21e0d
54Yunyun <102648992+54Yunyun@users.noreply.github.com>
2023-07-22 20:31:08 +0800

    增加刪除多筆、顯示代辦項目數目

--- src/App.vue
+++ src/App.vue
@@ -3,16 +3,18 @@ import { ref, onMounted, computed, watch } from 'vue';
 
 const todos = ref([]);
 const name = ref('');
-
+const todoCount = ref(0);
 const inputContent = ref('');
 const inputCategory = ref('');
 
+// 排序，最新的在最上面
 const todosAsc = computed(() =>
   todos.value.sort((a, b) => {
     return b.createAt - a.createAt;
   })
 );
 
+// 創建Todo物件
 const addTodo = () => {
   if (inputContent.value.trim() === '' || inputCategory.value === null) return;
   todos.value.push({
@@ -21,15 +23,30 @@ const addTodo = () => {
     createAt: new Date().getTime(),
     isDone: false,
   });
+  todoCount.value++;
   inputContent.value = '';
   inputCategory.value = null;
 };
 
+// 刪除單筆
 const removeTodo = (todo) => {
   const index = todos.value.indexOf(todo);
   todos.value.splice(index, 1);
+  todoCount.value--;
 };
 
+// 刪除已完成（可多筆）
+const deleteCompleted = () => {
+  for (let i = todos.value.length - 1; i >= 0; i--) {
+    const item = todos.value[i];
+    if (item.isDone) {
+      todos.value.splice(i, 1);
+      todoCount.value--;
+    }
+  }
+};
+
+// 監聽todos、name，並存入localStorage
 watch(
   todos,
   (newVal) => {
@@ -42,6 +59,7 @@ watch(name, (newVal) => {
   localStorage.setItem('name', newVal);
 });
 
+
 onMounted(() => {
   name.value = localStorage.getItem('name') || '';
   todos.value = JSON.parse(localStorage.getItem('todos')) || [];
@@ -52,7 +70,7 @@ onMounted(() => {
   <main class="app">
     <section class="greeting">
       <h2 class="title">
-        Hi <input type="text" placeholder="Type your name" v-model="name" />
+        Hi, <input type="text" placeholder="Type your name" v-model="name" />
       </h2>
     </section>
     <section class="create-todo">
@@ -69,8 +87,7 @@ onMounted(() => {
               value="business"
               v-model="inputCategory"
             />
-            <span class="bubble business">              
-            </span>
+            <span class="bubble business"> </span>
             <div>Business</div>
           </label>
           <label>
@@ -80,8 +97,7 @@ onMounted(() => {
               value="personal"
               v-model="inputCategory"
             />
-            <span class="bubble personal">            
-            </span>
+            <span class="bubble personal"> </span>
             <div>Personal</div>
           </label>
         </div>
@@ -89,7 +105,14 @@ onMounted(() => {
       </form>
     </section>
     <section class="todo-list">
-      <h3>TODO LIST</h3>
+      <div class="todo-list-wrap">
+        <h3>TODO LIST</h3>
+        <div class="delete-completed">
+          <button class="delete-completed-btn" @click="deleteCompleted()">
+            Delete completed items
+          </button>
+        </div>
+      </div>
       <div class="list">
         <div
           v-for="todo in todosAsc"
@@ -102,10 +125,18 @@ onMounted(() => {
           <div class="todo-content">
             <input type="text" v-model="todo.content" />
           </div>
+          <div class="createAt">
+            {{ new Date(todo.createAt).toLocaleString() }}
+          </div>
           <div class="actions">
             <button class="delete" @click="removeTodo(todo)">Delete</button>
           </div>
         </div>
+      </div>
+      <div class="todo-list-info">
+        <div class="todo-count">
+          <span>{{ todoCount }}</span> Items left
+        </div>      
       </div>
     </section>
   </main>
